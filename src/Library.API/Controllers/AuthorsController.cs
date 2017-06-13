dd938b9a87296006010072704cf3b77f19c84835
Ice <Ice@ICE-PC>
2017-06-13 23:31:54 +0530

    Added new controller. Added a method to get collection of authors. Added auto mapper

---
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System . Collections . Generic;
+using System . Linq;
+using System . Threading . Tasks;
+using Microsoft . AspNetCore . Mvc;
+using Library . API . Services;
+using AutoMapper;
+using Library . API . Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Library . API . Controllers
+{
+    [Route ( "api/authors" )]
+    public class AuthorsController : Controller
+    {
+        ILibraryRepository _repo;
+
+        public AuthorsController ( ILibraryRepository repo )
+        {
+            _repo = repo;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IActionResult Get ( )
+        {
+            var authors = _repo.GetAuthors();
+            var authorsDto = Mapper.Map<IEnumerable<AuthorDto>>(authors);
+            return new JsonResult ( authorsDto );
+        }
+
+        // GET api/values/5
+        [HttpGet ( "{id}" )]
+        public string Get ( int id )
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post ( [FromBody]string value )
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut ( "{id}" )]
+        public void Put ( int id , [FromBody]string value )
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete ( "{id}" )]
+        public void Delete ( int id )
+        {
+        }
+    }
+}
