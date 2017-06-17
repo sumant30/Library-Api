b23feb442d26335cbf96592346eba2c3a871ffe1
Ice <Ice@ICE-PC>
2017-06-17 17:27:39 +0530

    Posting and getting collection from api.

---
+++ src/Library.API/Controllers/AuthorsCollectionController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System . Collections . Generic;
+using System . Linq;
+using System . Threading . Tasks;
+using Microsoft . AspNetCore . Mvc;
+using Library . API . Services;
+using Library . API . Models;
+using AutoMapper;
+using Library . API . Entities;
+using Library . API . Helpers;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Library . API . Controllers
+{
+    [Route ( "api/authorscollection" )]
+    public class AuthorsCollectionController : Controller
+    {
+        ILibraryRepository _repo;
+
+        public AuthorsCollectionController ( ILibraryRepository repo )
+        {
+            _repo = repo;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get ( )
+        {
+            return new string [ ] { "value1" , "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet ( "({ids})",Name ="GetAuthorCollection" )]
+        public IActionResult Get ([ModelBinder(BinderType =typeof(ArrayModelBinder))] IEnumerable<Guid> ids )
+        {
+           if(ids == null )
+            {
+                return BadRequest ( );
+            }
+            var authorEntities = _repo.GetAuthors(ids);
+            if(ids.Count() != authorEntities . Count() )
+            {
+                return NotFound ( );
+            }
+            var authorDtos = Mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            return Ok ( authorDtos );
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult CreateAuthorCollection ( [FromBody]IEnumerable<AuthorForCreationDto> authorsDto)
+        {
+            if ( authorsDto == null )
+            {
+                return BadRequest ( );
+            }
+            var authorsEntity  = Mapper.Map<IEnumerable<Author>>(authorsDto);
+            authorsEntity . ToList ( ) . ForEach ( x => { _repo . AddAuthor ( x ); } );
+            if ( !_repo . Save ( ) )
+            {
+                throw new Exception ( "Creating an author collection failed on save." );
+            }
+            var authorDtos = Mapper.Map<IEnumerable<AuthorDto>>(authorsEntity);
+            var authorIds = string.Join(",",authorDtos.Select(x => x.Id));
+            return CreatedAtRoute ( "GetAuthorCollection" , new { ids = authorIds } , authorDtos );
+        }
+
+        // PUT api/values/5
+        [HttpPut ( "{id}" )]
+        public void Put ( int id , [FromBody]string value )
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete ( "{id}" )]
+        public void Delete ( int id )
+        {
+        }
+    }
+}
---
+++ src/Library.API/Helpers/ArrayModelBinder.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft . AspNetCore . Mvc . ModelBinding;
+using System;
+using System . Collections . Generic;
+using System . ComponentModel;
+using System . Linq;
+using System . Threading . Tasks;
+
+namespace Library.API.Helpers
+{
+    public class ArrayModelBinder : IModelBinder
+    {
+        public Task BindModelAsync ( ModelBindingContext bindingContext )
+        {
+            //As our binding only works on IEnumrable type
+            if ( !bindingContext . ModelMetadata . IsEnumerableType )
+            {
+                bindingContext . Result = ModelBindingResult . Failed();
+                return Task . CompletedTask;
+            }
+
+            //Get the inputted value through value provider
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).ToString();
+
+            //If that value is whitespace or null, we return null
+            if ( string . IsNullOrWhiteSpace ( value ) )
+            {
+                bindingContext . Result = ModelBindingResult . Success ( null );
+                return Task . CompletedTask;
+            }
+
+            //The value isn't null or whitespace,
+            //and the type of model is enumerable.
+            //Get the enumerabe's type, and a convertor
+            var elementType = bindingContext.ModelType.GetType().GenericTypeArguments[0];
+            var convertor = TypeDescriptor.GetConverter(elementType);
+
+            //Convert each item in the value list to enumerable type
+            var values = value
+                         .Split(new[] {","},StringSplitOptions.RemoveEmptyEntries)
+                         .Select(x => convertor.ConvertFromString(x.Trim()))
+                         .ToArray();
+
+            //Create an array of that type, and set it as Model value
+            var typedValues = Array.CreateInstance(elementType,values.Length);
+            values . CopyTo ( typedValues , 0 );
+            bindingContext . Model = typedValues;
+
+            //return a successful result passing the Model
+            bindingContext . Result = ModelBindingResult . Success ( typedValues );
+            return Task . CompletedTask;
+                            
+        }
+    }
+}
